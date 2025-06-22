598a9295b51e955633d799a2c5e4482769da422a
katerinacejas <katerinacejas3@gmail.com>
2025-06-22 15:21:55 -0300

    n_reinas_backtracking

--- algoritmos_backtracking/n_reinas.py
+++ algoritmos_backtracking/n_reinas.py
@@ -6,17 +6,47 @@ condiciones:
 	2) una reina come a otra si estan en la misma: fila, columna, diagonal
 backtracking: si no se puede poner una reaina en una fija. se vuelve atrás.
 caso base: si hay lamisma cantidad de reinas puestas que el tamaño del tablero (N)
-array = [2,1,0,3] es equivalente a | 0 0 1 0 |
-                                   | 0 1 0 0 |
+array = [2,1,0,3] es equivalente a | 0 0 1 0 |  = columna 0 1 2 3
+                                   | 0 0 0 0 |       fila 2 3 0 3
                                    | 1 0 0 0 |
-								   | 0 0 0 1 |
+								   | 0 1 0 1 |
 """
 def n_reinas(array, fila):
-	array.append(fila) # ponemos una reina en la fila de la columna actual
-	if()
-
+	# caso base: si ya llene todo el array
+	if len(array)-1 == fila:
+		imprimir_tablero(array, len(array))
+		return 
 	
+	for columna in range(len(array)):
+		if verificar_filas(array, fila) and verificar_diagonales(array, fila, columna):
+			array.append(fila) # ponemos una reina
+			print("puse una reina")
+			n_reinas(array, fila+1)
+			array.pop() # sacamos una reina. backtracking
 
+def verificar_filas(array, fila):
+	print("ingrese a verificar filas")
+	for columna in range(len(array)):
+		if array[columna] == fila: 
+			return False
+	return True
 
-array = [0] * 8 # tablero de 8x8 
+def verificar_diagonales(array, fila, columna):
+	for i in range(len(array)):
+		if abs(array[i]-columna) == abs(i - fila):
+			return False
+	return True
+
+def imprimir_tablero(tablero, n):
+    for fila in range(n):
+        linea = ''
+        for col in range(n):
+            if tablero[fila] == col:
+                linea += 'Q '
+            else:
+                linea += '. '
+        print(linea)
+    print() 
+
+array = [0,0,0,0] # tablero de 4x4
 n_reinas(array, 0)
